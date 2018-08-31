f563883fc2cbba908c59dc168418293041de7ac2
Abraham De Alba <deal1211@m506-118-26434.local>
2018-08-31 08:16:58 -0700

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Abraham De Alba on 8/31/18.
+//  Copyright © 2018 Abraham De Alba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
